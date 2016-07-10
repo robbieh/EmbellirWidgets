a363a8b0c365f66f017d19e037c72191bd9aa4cd
Robbie Huffman <robbie.huffman@gmail.com>
2016-07-09 23:31:13 -0400

    Basic clock works, updating every minute

--- app/src/main/java/nundrum/embellirwidgets/EmbellirClock.java
+++ app/src/main/java/nundrum/embellirwidgets/EmbellirClock.java
@@ -1,13 +1,17 @@
 package nundrum.embellirwidgets;
 
+import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.appwidget.AppWidgetProviderInfo;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -34,8 +38,6 @@ public class EmbellirClock extends AppWidgetProvider {
     }
 
 
-
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         Log.v(TAG, "onUpdate called");
@@ -43,6 +45,9 @@ public class EmbellirClock extends AppWidgetProvider {
         final AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         final Intent clockIntent = new Intent(context,  EmbellirClockUpdateService.class);
         final Calendar now = Calendar.getInstance();
+        for (int appWidgetId : appWidgetIds) {
+            getWidgetSizes(context, appWidgetManager, appWidgetId);
+        }
 
         if (service  == null) {
             service = PendingIntent.getService(context, 0, clockIntent, PendingIntent.FLAG_CANCEL_CURRENT);
@@ -65,9 +70,75 @@ public class EmbellirClock extends AppWidgetProvider {
             appWidgetManager.updateAppWidget(appWidgetId, views);
             updateAppWidget(context, appWidgetManager, appWidgetId);
 
-            //Bitmap bitmap = Bitmap.createBitmap()
-            //Canvas canvas = new Canvas();
+
         }*/
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+        getWidgetSizes(context, appWidgetManager, appWidgetId);
+    }
+
+    private void getWidgetSizes(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        int mWidgetLandWidth;
+        int mWidgetPortHeight;
+        int mWidgetPortWidth;
+        int mWidgetLandHeight;
+        boolean mIsKeyguard;
+        //context.getResources().getBoolean(R.bool.isPort);
+
+
+           AppWidgetProviderInfo providerInfo = AppWidgetManager.getInstance(
+                   context).getAppWidgetInfo(appWidgetId);
+           mWidgetLandWidth = providerInfo.minWidth;
+           mWidgetPortHeight = providerInfo.minHeight;
+           mWidgetPortWidth = providerInfo.minWidth;
+           mWidgetLandHeight = providerInfo.minHeight;
+           Bundle mAppWidgetOptions = null;
+           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+               mAppWidgetOptions = appWidgetManager.getAppWidgetOptions(appWidgetId);
+            if (mAppWidgetOptions != null
+                    && mAppWidgetOptions
+                    .getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH) > 0) {
+                    Log.d(TAG,"appWidgetOptions not null, getting widget sizes...");
+                // Reduce width by a margin of 8dp (automatically added by
+                // Android, can vary with third party launchers)
+
+           /* Actually Min and Max is a bit irritating,
+              because it depends on the homescreen orientation
+              whether Min or Max should be used: */
+
+                mWidgetPortWidth = mAppWidgetOptions
+                        .getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+                mWidgetLandWidth = mAppWidgetOptions
+                        .getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
+                mWidgetLandHeight = mAppWidgetOptions
+                        .getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+                mWidgetPortHeight = mAppWidgetOptions
+                        .getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
+
+                // Get the value of OPTION_APPWIDGET_HOST_CATEGORY
+                int category = mAppWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY, -1);
+
+                // If the value is WIDGET_CATEGORY_KEYGUARD, it's a lockscreen
+                // widget (dumped with Android-L preview :-( ).
+                mIsKeyguard = category == AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD;
+
+            } else {
+               Log.d(TAG,"No AppWidgetOptions for this widget, using minimal dimensions from provider info!");
+                // For some reason I had to set this again here, may be obsolete
+                mWidgetLandWidth = providerInfo.minWidth;
+                mWidgetPortHeight = providerInfo.minHeight;
+                mWidgetPortWidth = providerInfo.minWidth;
+                mWidgetLandHeight = providerInfo.minHeight;
+            }
+            Log.v(TAG,String.valueOf(mWidgetPortWidth));
+            Log.v(TAG,String.valueOf(mWidgetLandWidth));
+            Log.v(TAG,String.valueOf(mWidgetPortHeight));
+            Log.v(TAG,String.valueOf(mWidgetLandHeight));
+
+
     }
 
     @Override
--- app/src/main/java/nundrum/embellirwidgets/EmbellirClockUpdateService.java
+++ app/src/main/java/nundrum/embellirwidgets/EmbellirClockUpdateService.java
@@ -1,20 +1,38 @@
 package nundrum.embellirwidgets;
 
-import android.app.AlarmManager;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.RectF;
 import android.os.IBinder;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.widget.RelativeLayout;
 import android.widget.RemoteViews;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class EmbellirClockUpdateService extends Service {
     private static final String TAG = "EmbellirClockUpdateServ";
+
+    private Bitmap bitmap;
+    private Canvas canvas;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private final int color_main = Color.rgb(50, 170, 70);
+    private final int color_hilight = Color.rgb(50, 140, 50);
+
+    private final float degPer60 = 360.0f / 60.0f;
+    private final float degPer12 = 360.0f / 12.0f;
+    private final float degPer24 = 360.0f / 24.0f;
+
 
     //public EmbellirClockUpdateService() {
     //}
@@ -35,8 +53,11 @@ public class EmbellirClockUpdateService extends Service {
         }*/
         String lastUpdated = DateFormat.format("hh:mm a", new Date()).toString();
 
+        drawClock();
+
         RemoteViews view = new RemoteViews(getPackageName(), R.layout.embellir_clock);
-        view.setTextViewText(R.id.appwidget_text, lastUpdated);
+        view.setTextViewText(R.id.clock_text, lastUpdated);
+        view.setImageViewBitmap(R.id.clock_image, bitmap);
 
         ComponentName thisWidget = new ComponentName(this, EmbellirClock.class);
         AppWidgetManager manager = AppWidgetManager.getInstance(this);
@@ -49,5 +70,79 @@ public class EmbellirClockUpdateService extends Service {
         // TODO: Return the communication channel to the service.
         //throw new UnsupportedOperationException("Not yet implemented");
         return null;
+    }
+
+    private void drawClock(){
+        if (null == bitmap) {
+
+            bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+        }
+        canvas = new Canvas(bitmap);
+
+        canvas.save();
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+
+        paint.setStrokeWidth(5);
+
+        float w = canvas.getWidth();
+        float h = canvas.getHeight();
+        float size = Math.min(w, h);
+        float radius = 1.0f * size;
+        float halfRadius = 0.5f * radius;
+
+        // get time/date info
+        Calendar now = Calendar.getInstance();
+        //Calendar nowGMT = Calendar.getInstance(TZ, locale);
+        int hour = now.get(Calendar.HOUR);
+        int hour24 = now.get(Calendar.HOUR_OF_DAY);
+        int minute = now.get(Calendar.MINUTE);
+
+        float riseDeg = 0.0f;
+        float setDeg = 0.0f;
+
+
+
+        float minuteDeg =  minute * degPer60;
+        float hourDeg = hour * degPer12;
+        float hour24Deg = hour24 * degPer24 + minute / degPer60 / degPer24;
+
+        float ringmax = (radius * 0.2f); //divide into 10 rings
+        float hp = (ringmax * 4f);  //hour
+        float mp = (ringmax * 3f);  //minute
+        float ssp = (ringmax * 5f); //sunrise/sunset/24h
+        float cp = (ringmax * 1f) - (ringmax * 0.5f);  //calendar
+        float dp = (ringmax * 1f) + (ringmax * 0.5f);  //day
+
+        canvas.translate(0.5f * w, 0.5f * h );
+        canvas.rotate(-90);
+
+        paint.setStyle(Paint.Style.STROKE);
+
+         //24h marker
+        //mPaint.setColor(mCSHl);
+        paint.setStrokeWidth(ringmax - (ringmax * 0.5f));
+        //drawArc(canvas, 0.0f + ssp - radius, 0.0f + ssp - radius, radius - ssp, radius - ssp, hour24Deg - 3f , 6f ,false, paint);
+        //hour ring
+        //mPaint.setColor(mCPHlm00);
+        paint.setColor(color_hilight);
+        paint.setStrokeWidth(ringmax / 1.5f);
+        drawArc(canvas, 0.0f + hp - radius, 0.0f + hp - radius, radius - hp, radius - hp, 0.0f, hourDeg, false, paint);
+        //minute ring
+        //mPaint.setColor(mCPMain);
+        paint.setColor(color_main);
+        paint.setStrokeWidth(ringmax / 2.5f);
+        drawArc(canvas, 0.0f + mp - radius, 0.0f + mp - radius, radius - mp, radius - mp, 0.0f, minuteDeg,false, paint);
+
+
+        canvas.restore();
+    }
+
+    private void drawArc(Canvas canvas, float left, float top, float right, float bottom, float startAngle, float sweepAngle, boolean useCenter, Paint paint) {
+        //float radius = (right - left) / 2.0f ;
+        final RectF oval = new RectF();
+        oval.set(left, top, right, bottom);
+        Path myPath = new Path();
+        myPath.arcTo(oval, startAngle, sweepAngle, true);
+        canvas.drawPath(myPath, paint);
     }
 }
